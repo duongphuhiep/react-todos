98e869540e14f8065927350e4658efe754475305
duong <duongphuhiep@gmail.com>
2016-05-15 03:27:37 +0200

    add fetch api

---
+++ src/__tests__/fetch.test.js
@@ -0,0 +1,61 @@
+jest.unmock('isomorphic-fetch')
+import fetch from 'isomorphic-fetch'
+
+describe("Hello fetch", ()=> {
+
+  var handleResponse;
+
+  beforeEach(() => {
+    handleResponse = {
+      success: (response) => console.log(response),
+      fail: (err) => console.log(err)
+    }
+    spyOn(handleResponse, 'success');
+    spyOn(handleResponse, 'fail');
+  })
+
+  xit("should get hcqh posts", (done)=> {
+    fetch("http://hopcaquehuong.org/backend/blog.php?page=1&lang=fr")
+      .then(response => {
+        expect(response.ok).toBe(true)
+        if (!response.ok) throw new Error(response.status + " "+response.statusText);
+        return response;
+      })
+      .then(response => response.json())
+      .then(json => {
+        expect(json.page).toBe(1)
+        handleResponse.success(json);
+      })
+      .catch(err=> {
+        handleResponse.fail(err);
+      })
+      .then(()=>{
+        expect(handleResponse.success.calls.any()).toBe(true)
+        expect(handleResponse.fail.calls.any()).toBe(false)
+        done()
+      })
+  })
+
+  it("should get 404", (done)=> {
+    fetch("http://hopcaquehuong.org/backend/foo.json")
+      .then(response => {
+        expect(response.ok).toBe(false)
+
+        if (!response.ok) throw new Error(response.status + " "+response.statusText);
+        return response;
+      })
+      .then(response => response.json())
+      .then(json => {
+        handleResponse.success(json);
+      })
+      .catch(err=> {
+        handleResponse.fail(err);
+      })
+      .then(()=>{
+        expect(handleResponse.success.calls.any()).toBe(false)
+        expect(handleResponse.fail.calls.any()).toBe(true)
+        done()
+      })
+  })
+
+})
