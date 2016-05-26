5f250195f04c950b4ff8dc416ce1dd5ad1ab7ed2
duong <duongphuhiep@gmail.com>
2016-05-26 02:47:25 +0200

    try pathset

--- src/__tests__/falcor.test.js
+++ src/__tests__/falcor.test.js
@@ -2,29 +2,37 @@ jest.unmock("falcor")
 import {Model as FalcorModel} from "falcor"
 
 describe("falcor model", ()=>{
-  it("should async", ()=>{
 
-    let model = new FalcorModel({
-      cache: {
-        stock: [
-          {
-            name: "iphone",
-            category: "smartphone",
-            price: 600
-          },
-          {
-            name: "nexus",
-            category: "smartphone",
-            price: 400
-          }
-        ]
-      }
-    })
+  let model = new FalcorModel({
+    cache: {
+      stock: [
+        {
+          name: "iphone",
+          category: "smartphone",
+          price: 600
+        },
+        {
+          name: "nexus",
+          category: "smartphone",
+          price: 400
+        }
+      ]
+    }
+  })
 
-    model.getValue("stock[1].name").then((v)=>{
+
+  pit("get value should async", ()=>{
+    return model.getValue("stock[1].name").then((v)=>{
       expect(v).toEqual("nexus")
     })
-
   })
+
+  pit("get value set should async", ()=>{
+    return model.get("stock[0..1]['name','price']").then((v)=>{
+      expect(v.json.stock[0].price).toEqual(600)
+      expect(v.json.stock[1].name).toEqual("nexus")
+    })
+  })
+  
 })
 
